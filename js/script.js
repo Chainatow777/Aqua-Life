c3f4de7555055d8a55b89be7b77fc0088da19059
Chainatow777 <samuelldesouza17@gmail.com>
2022-07-11 21:29:16 -0300

    ag vai

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
 function onload() {
 console.log('teu pão de queijo é meu :)');
     let text = document.getElementById('text');
-    // let bird1 = document.getElementById('bird1');
-    // let bird2 = document.getElementById('bird2');
     let btn = document.getElementById('btn');
 
     window.addEventListener('scroll', function () {
         let value = window.scrollY;
 
         text.style.top = 35 + value * -0.1 + '%';
-        // bird1.style.top = value * -1.5 + 'px';
-        // bird1.style.left = value * 2 + 'px';
-        // bird2.style.top = value * -1.5 + 'px';
-        // bird2.style.left = value * -5 + 'px';
 
     })
 
-    let home = document.getElementById('about');
+    let home = document.getElementById('home');
    home.classList.add('aberto');
    setTimeout(() => { home.classList.remove('aberto');}, 2500);
+}
+
+function likeButton (){
+    let heart = document.querySelector('.heart');
+    let likes = document.querySelector('.likes');
+    if (heart.scroll.match("../image/heart.png")){
+        heart.src = "../image/heart_red.png";
+        likes.innerHTML = "1,285 likes";
+    }
+    else {
+        heart.src = "../image/heart.png"
+        likes.innerHTML = "1,283 likes";
+    }
 }
